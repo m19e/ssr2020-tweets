8ddbc09682ecb712d6822072d0f2be8320ff689e
m19e <nexus.me7290@gmail.com>
2020-12-04 00:36:38 +0900

    Add units util

---
+++ utils/units.ts
@@ -0,0 +1,10 @@
+import { Unit } from "../interfaces";
+
+export const UnitData: { [name: string]: Unit } = {
+    ill: { name: "illuminationstars", color: "#ffe462" },
+    lan: { name: "lantica", color: "#e3b1fa" },
+    hok: { name: "hokagoclimaxgirls", color: "#ffa95c" },
+    als: { name: "alstroemeria", color: "#ffabc9" },
+    str: { name: "straylight", color: "#ffb3b6" },
+    noc: { name: "noctchill", color: "#a9bbfd" },
+};
