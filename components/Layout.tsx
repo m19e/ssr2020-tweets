6dc3e3e3ccf951b062942a2d342c7e16162f5cd5
m19e <nexus.me7290@gmail.com>
2020-12-04 00:38:54 +0900

    Wrap children by neumo-card

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,19 +2,22 @@ import React, { ReactNode } from "react";
 import Link from "next/link";
 import Head from "next/head";
 
+import { Card } from "ui-neumorphism";
+import "ui-neumorphism/dist/index.css";
+
 type Props = {
     children?: ReactNode;
     title?: string;
 };
 
 const Layout = ({ children, title = "This is the default title" }: Props) => (
-    <div>
+    <div style={{ backgroundColor: "#e4ebf5" }}>
         <Head>
             <title>{title}</title>
             <meta charSet="utf-8" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-        <header>
+        {/* <header>
             <nav>
                 <Link href="/">
                     <a>Home</a>
@@ -32,14 +35,18 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
                     <a>Tweets</a>
                 </Link>{" "}
             </nav>
-        </header>
+        </header> */}
         <div style={{ width: "100%" }}>
-            <div style={{ flex: 1 }}>{children}</div>
+            <div style={{ flex: 1 }}>
+                <Card inset style={{ margin: "16px", padding: "16px", paddingTop: "8px" }}>
+                    {children}
+                </Card>
+            </div>
         </div>
-        <footer>
+        {/* <footer>
             <hr />
             <span>I'm here to stay (Footer)</span>
-        </footer>
+        </footer> */}
     </div>
 );
 
