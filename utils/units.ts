a82082a0e4375a2afedccf6a01d729729d08c349
m19e <nexus.me7290@gmail.com>
2020-12-04 04:03:12 +0900

    Update unit color

--- utils/units.ts
+++ utils/units.ts
@@ -1,10 +1,10 @@
 import { Unit } from "../interfaces";
 
 export const UnitData: { [name: string]: Unit } = {
-    ill: { name: "illuminationstars", color: "#ffe462" },
-    lan: { name: "lantica", color: "#e3b1fa" },
-    hok: { name: "hokagoclimaxgirls", color: "#ffa95c" },
-    als: { name: "alstroemeria", color: "#ffabc9" },
-    str: { name: "straylight", color: "#ffb3b6" },
+    ill: { name: "illuminationstars", color: "#ffa700" },
+    lan: { name: "lantica", color: "#853998" },
+    hok: { name: "hokagoclimaxgirls", color: "#fa8333" },
+    als: { name: "alstroemeria", color: "#e43b80" },
+    str: { name: "straylight", color: "#e71212" },
     noc: { name: "noctchill", color: "#a9bbfd" },
 };
