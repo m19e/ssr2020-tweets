6d7c59b4495b9fe74f36e4cb8177a8945752f069
m19e <nexus.me7290@gmail.com>
2020-12-04 00:47:55 +0900

    Set loading box

--- pages/tweets/index.tsx
+++ pages/tweets/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from "next";
 import Link from "next/link";
 import { TwitterTweetEmbed } from "react-twitter-embed";
+import { Card, Divider } from "ui-neumorphism";
 
 import { Tweet } from "../../interfaces";
 import { TweetData } from "../../utils/tweets";
@@ -12,19 +13,26 @@ type Props = {
 
 const WithStaticProps = ({ items }: Props) => (
     <Layout title="Tweets | Next.js + TypeScript Example">
-        <h1>Tweets</h1>
-        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around" }}>
+        {/* <h1 style={{ fontSize: "60px", padding: "0 8px" }}>Tweets</h1> */}
+        <Divider dense elevated style={{ margin: "8px 0" }} />
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
             {items.map((item: Tweet) => (
                 <div style={{ padding: "0 8px" }}>
-                    <TwitterTweetEmbed tweetId={item.id} options={{ cards: "hidden", width: 300, maxWidth: 800 }} placeholder={"Loading..."} />
+                    <div style={{ width: "250px", height: "500px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <TwitterTweetEmbed
+                            tweetId={item.id}
+                            options={{ width: 300, maxWidth: 800 }}
+                            placeholder={<Card style={{ backgroundColor: item.idol.unit.color, borderRadius: "5%", width: "100px", height: "100px" }}></Card>}
+                        />
+                    </div>
                 </div>
             ))}
         </div>
-        <p>
+        {/* <p>
             <Link href="/">
                 <a>Go home</a>
             </Link>
-        </p>
+        </p> */}
     </Layout>
 );
 
