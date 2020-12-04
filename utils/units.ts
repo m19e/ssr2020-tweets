4fff3eac7a26fa564d24c7015e0edcee5d9af908
m19e <nexus.me7290@gmail.com>
2020-12-05 00:22:50 +0900

    Add unitinfolist

--- utils/units.ts
+++ utils/units.ts
@@ -1,4 +1,4 @@
-import { Unit } from "../interfaces";
+import { Unit, UnitInfo } from "../interfaces";
 
 export const UnitData: { [name: string]: Unit } = {
     ill: { name: "illuminationstars", color: "#ffa700" },
@@ -8,3 +8,12 @@ export const UnitData: { [name: string]: Unit } = {
     str: { name: "straylight", color: "#e71212" },
     noc: { name: "noctchill", color: "#a9bbfd" },
 };
+
+export const UnitInfoList: UnitInfo[] = [
+    { name: "illuminationstars", fullname: "illumination STARS", title: "イルミネ" },
+    { name: "lantica", fullname: "L'Antica", title: "アンティーカ" },
+    { name: "hokagoclimaxgirls", fullname: "放課後クライマックスガールズ", title: "放クラ" },
+    { name: "alstroemeria", fullname: "ALSTROEMERIA", title: "アルスト" },
+    { name: "straylight", fullname: "Straylight", title: "ストレイ" },
+    { name: "noctchill", fullname: "noctchill", title: "ノクチル" },
+];
