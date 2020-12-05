b3266b4b93a31a323c0067c5a3c3e9a02116b3c8
m19e <nexus.me7290@gmail.com>
2020-12-05 10:35:19 +0900

    Replace head

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import Head from "next/head";
+import Head from "../components/Head";
 
 import { Card } from "ui-neumorphism";
 import "ui-neumorphism/dist/index.css";
@@ -11,12 +11,7 @@ type Props = {
 
 const Layout = ({ children, title = "This is the default title" }: Props) => (
     <div>
-        <Head>
-            <title>{title}</title>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <style>{`html { background-color: #e4ebf5 }`}</style>
-        </Head>
+        <Head title={title} />
         <div style={{ width: "100%" }}>
             <div style={{ flex: 1 }}>
                 <Card inset style={{ margin: "32px", padding: "16px", paddingTop: "8px" }}>
